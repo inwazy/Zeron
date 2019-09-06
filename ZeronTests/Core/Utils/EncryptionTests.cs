6d1de6694348b8404937ebf8fdf286f201890960
= <jiowcl@gmail.com>
2019-09-06 20:42:43 +0800

    Add a unit tests

---
+++ ZeronTests/Core/Utils/EncryptionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zeron.Core.Utils.Tests
+{
+    [TestClass()]
+    public class EncryptionTests
+    {
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            string userNamePayload = Encryption.Encrypt("Ji-Feng Tsai");
+
+            Assert.IsNotNull(userNamePayload);
+        }
+
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            string userName = Encryption.Decrypt("JozAlZESAof1SwpzMSMvIQ==");
+
+            Assert.AreEqual(userName, "Ji-Feng Tsai");
+        }
+    }
+}
